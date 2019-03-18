e8c0af011977e6c7a2daf42a3c166ce3b37b043f
rcornu <contact@romaincornu.com>
2019-03-18 15:14:28 +0100

    update script

--- script.sh
+++ script.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-if [[ -z "${PAYLOAD}" ]]; then
+if test -z "$PAYLOAD"; then
+  echo 'No payload given'
+else
   echo $PAYLOAD
   DATA=$(echo $PAYLOAD | base64 -d | gunzip)
   echo $DATA | jq -r > /run.sh
   chmod +x /run.sh
   /run.sh
-else
-  echo 'No payload given'
+
 fi

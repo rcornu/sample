6f795cf9b86cdc30ce51a1d9e8a0cacd3bea9d2a
rcornu <contact@romaincornu.com>
2019-03-18 11:33:45 +0100

    check for payload

--- script.sh
+++ script.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-DATA=$(echo $PAYLOAD | base64 -d | gunzip)
-echo $DATA | jq -r > /run.sh
-chmod +x /run.sh
-/run.sh
+if [[ -z "${PAYLOAD}" ]]; then
+  DATA=$(echo $PAYLOAD | base64 -d | gunzip)
+  echo $DATA | jq -r > /run.sh
+  chmod +x /run.sh
+  /run.sh
+fi
+
+
